53b6e816f240253de94b14b9e7a2b732b2ba3b48
yashxog <sonaryash70@gmail.com>
2023-11-01 19:14:31 +0530

    "eslint started properly removed all errors"

--- src/config.ts
+++ src/config.ts
@@ -4,38 +4,36 @@ import bunyan from 'bunyan';
 dotenv.config({});
 
 class Config {
-    public DATABASE_URL: string | undefined;
-    public JWT_TOKEN: string | undefined;
-    public NODE_ENV: string | undefined;
-    public SECRETE_KEY_1: string | undefined;
-    public SECRETE_KEY_2: string | undefined;
-    public CLIENT_URL: string | undefined;
-    public REDIS_HOST: string | undefined;
+  public DATABASE_URL: string | undefined;
+  public JWT_TOKEN: string | undefined;
+  public NODE_ENV: string | undefined;
+  public SECRETE_KEY_1: string | undefined;
+  public SECRETE_KEY_2: string | undefined;
+  public CLIENT_URL: string | undefined;
+  public REDIS_HOST: string | undefined;
 
-    private DEFAULT_DATABASE_URL = 'mongodb://0.0.0.0/nxsite-stream-backend';
+  private DEFAULT_DATABASE_URL = 'mongodb://0.0.0.0/nxsite-stream-backend';
 
-    constructor() {
-        this.DATABASE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
-        this.JWT_TOKEN = process.env.JWT_TOKEN || '1234',
-        this.NODE_ENV = process.env.NODE_ENV || '';
-        this.SECRETE_KEY_1 = process.env.SECRETE_KEY_1 || '';
-        this.SECRETE_KEY_2 = process.env.SECRETE_KEY_2 || '';
-        this.CLIENT_URL = process.env.CLIENT_URL || '';
-        this.REDIS_HOST = process.env.REDIS_HOST || '';
+  constructor() {
+    this.DATABASE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
+    (this.JWT_TOKEN = process.env.JWT_TOKEN || '1234'), (this.NODE_ENV = process.env.NODE_ENV || '');
+    this.SECRETE_KEY_1 = process.env.SECRETE_KEY_1 || '';
+    this.SECRETE_KEY_2 = process.env.SECRETE_KEY_2 || '';
+    this.CLIENT_URL = process.env.CLIENT_URL || '';
+    this.REDIS_HOST = process.env.REDIS_HOST || '';
+  }
+
+  public createLogger(name: string): bunyan {
+    return bunyan.createLogger({ name, level: 'debug' });
+  }
+
+  public validateConfig(): void {
+    for (const [key, value] of Object.entries(this)) {
+      if (value === undefined) {
+        throw new Error(`Configuration ${key} is undefined`);
+      }
     }
-
-    public createLogger(name: string): bunyan {
-        return bunyan.createLogger({name, level: 'debug'});
-    }
-
-
-    public validateConfig(): void {
-        for(const [key, value] of Object.entries(this)) {
-            if(value === undefined) {
-                throw new Error(`Configuration ${key} is undefined`);
-            }
-        }
-    }
-};
+  }
+}
 
 export const config: Config = new Config();
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,4 +1,4 @@
-import {Application, json, urlencoded, Response, Request, NextFunction} from 'express';
+import { Application, json, urlencoded, Response, Request, NextFunction } from 'express';
 import http from 'http';
 import cors from 'cors';
 import compression from 'compression';
@@ -19,96 +19,96 @@ const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
 
 export class NxsiteServer {
-    private app: Application;
+  private app: Application;
 
-    constructor(app: Application) {
-        this.app = app;
-    };
+  constructor(app: Application) {
+    this.app = app;
+  }
 
-    public start(): void {
-        this.sequrityMiddleware(this.app);
-        this.standardMiddleware(this.app);
-        this.routesMiddleware(this.app);
-        this.globalErrorHandler(this.app);
-        this.startServer(this.app);
-    };
+  public start(): void {
+    this.sequrityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
 
-    private sequrityMiddleware(app: Application): void{
-        app.use(
-            cookieSession({
-                name: 'session',
-                keys: [config.SECRETE_KEY_1!, config.SECRETE_KEY_2!],
-                maxAge: 24 * 7 * 3600000,
-                secure: config.NODE_ENV !== 'development'
-            })
-        );
-        app.use(hpp());
-        app.use(helmet());
-        app.use(
-            cors({
-                origin: config.CLIENT_URL ,
-                credentials: true,
-                optionsSuccessStatus: 200,
-                methods: ['GET', 'POST', 'PUT', 'UPDATE', 'DELETE'],
-            })
-        );
-    };
+  private sequrityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: [config.SECRETE_KEY_1!, config.SECRETE_KEY_2!],
+        maxAge: 24 * 7 * 3600000,
+        secure: config.NODE_ENV !== 'development'
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: config.CLIENT_URL,
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'UPDATE', 'DELETE']
+      })
+    );
+  }
 
-    private standardMiddleware(app: Application): void{
-        app.use(compression());
-        app.use(json({ limit: '50mb' }));
-        app.use(urlencoded({extended: true, limit: '50mb'}));
-    };
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
 
-    private routesMiddleware(app: Application): void{
-        applicationRoutes(app);
-    };
+  private routesMiddleware(app: Application): void {
+    applicationRoutes(app);
+  }
 
-    private globalErrorHandler(app: Application): void{
-        app.all('*', (req: Request, res: Response) => {
-            res.status(HTTP_STATUS.NOT_FOUND).json({message: `${req.originalUrl} not found`});
-        });
+  private globalErrorHandler(app: Application): void {
+    app.all('*', (req: Request, res: Response) => {
+      res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found` });
+    });
 
-        app.use((error: IErrorResponse, req: Request, res: Response, next: NextFunction) => {
-            log.error(error);
-            if(error instanceof CustomError) {
-                return res.status(error.statusCode).json(error.serializeErrors());
-            }
-            next();
-        });
-    };
+    app.use((error: IErrorResponse, req: Request, res: Response, next: NextFunction) => {
+      log.error(error);
+      if (error instanceof CustomError) {
+        return res.status(error.statusCode).json(error.serializeErrors());
+      }
+      next();
+    });
+  }
 
-    private async startServer(app: Application): Promise<void> {
-        try {
-            const httpServer: http.Server = new http.Server(app);
-            const socketIO: Server = await this.createSocketIO(httpServer);
-            this.startHttpServer(httpServer);
-            this.socketIOConnections(socketIO);
-        } catch (error) {
-            log.error(error);
-        }
-    };
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      const socketIO: Server = await this.createSocketIO(httpServer);
+      this.startHttpServer(httpServer);
+      this.socketIOConnections(socketIO);
+    } catch (error) {
+      log.error(error);
+    }
+  }
 
-    private async createSocketIO(httpServer: http.Server): Promise<Server>{
-        const io: Server = new Server(httpServer, {
-            cors: {
-                origin: config.CLIENT_URL,
-                methods: ['GET', 'POST', 'PUT', 'UPDATE', 'DELETE'],
-            }
-        });
-        const pubClient = createClient({url: config.REDIS_HOST});
-        const subClient = pubClient.duplicate();
-        await Promise.all([pubClient.connect(), subClient.connect()]);
-        io.adapter(createAdapter(pubClient, subClient));
-        return io;
-    };
+  private async createSocketIO(httpServer: http.Server): Promise<Server> {
+    const io: Server = new Server(httpServer, {
+      cors: {
+        origin: config.CLIENT_URL,
+        methods: ['GET', 'POST', 'PUT', 'UPDATE', 'DELETE']
+      }
+    });
+    const pubClient = createClient({ url: config.REDIS_HOST });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    io.adapter(createAdapter(pubClient, subClient));
+    return io;
+  }
 
-    private startHttpServer(httpServer: http.Server): void{
-            log.info(`Server has sytarted with process ${process.pid}`);
-            httpServer.listen(SERVER_PORT, () => {
-            log.info(`Server running on port ${SERVER_PORT}`);
-        });
-    };
+  private startHttpServer(httpServer: http.Server): void {
+    log.info(`Server has sytarted with process ${process.pid}`);
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`Server running on port ${SERVER_PORT}`);
+    });
+  }
 
-    private socketIOConnections(io: Server):  void{}
+  private socketIOConnections(io: Server): void {}
 }
