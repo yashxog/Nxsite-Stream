692fd7cd1d26d539213fd7c10015dc940d700999
yashxog <sonaryash70@gmail.com>
2023-11-20 11:43:33 +0530

    "forget password functionality"

---
+++ src/shared/services/queues/email.queue.ts
@@ -0,0 +1,16 @@
+import { emailWorker } from '@root/shared/workers/emai.workers';
+import { BaseQueue } from '@services/queues/base.queue';
+import { IEmailJob } from '@user/interfaces/user.interface';
+
+class EmailQueue extends BaseQueue {
+  constructor() {
+    super('emails');
+    this.processJob('forgotPasswordEmail', 5, emailWorker.addNotificationEmail);
+  }
+
+  public addEmailJob(name: string, data: IEmailJob): void {
+    this.addJob(name, data);
+  }
+ }
+
+export const emailQueue: EmailQueue = new EmailQueue();
